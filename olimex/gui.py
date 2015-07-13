84d3ee2d645fd011f9be8b29c7bcdccada4651e7
Paul Logston <code@logston.me>
2015-07-13 00:59:22 -0400

    Make the GUI look more like an EKG

--- olimex/gui.py
+++ olimex/gui.py
@@ -1,74 +1,92 @@
 """
 This module defines logic for plotting exg data in real-time.
 """
-import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
+import scipy.ndimage
 import serial
-from olimex.constants import DEFAULT_BAUDRATE
 
+from olimex.constants import DEFAULT_BAUDRATE
 from olimex.exg import PacketStreamReader
 from olimex.mock import FakeSerialByteArray
-from olimex.utils import calculate_heart_rate
 
 
-def axes_refresher_generator(axes, reader):
+STRIP_LENGTH_SECONDS = 6
+DOTS_PER_SECOND = 250
+DOTS_PER_02_SECOND = DOTS_PER_SECOND / 5
+DOTS_PER_004_SECOND = DOTS_PER_SECOND / 25
+DOTS_PER_STRIP_LENGTH = DOTS_PER_SECOND * STRIP_LENGTH_SECONDS
+DOTS_PER_STRIP_HEIGHT = 1025
+DOTS_MAX_GRAPH_HEIGHT = 1023
+
+REFRESHES_PER_SECOND = 25
+REFRESH_INTERVAL_MS = 1000 / REFRESHES_PER_SECOND
+DOTS_TO_JUMP_PER_REFRESH = DOTS_PER_SECOND / REFRESHES_PER_SECOND
+
+mpl.rcParams['savefig.dpi'] = 600
+mpl.rcParams['savefig.bbox'] = 'tight'
+mpl.rcParams['lines.linewidth'] = 0.35
+
+fig, axes = plt.subplots(figsize=(STRIP_LENGTH_SECONDS, STRIP_LENGTH_SECONDS / 3),
+                         dpi=DOTS_PER_SECOND)
+
+axes.set_ylim(0, DOTS_PER_STRIP_HEIGHT)
+axes.set_xlim(0, DOTS_PER_STRIP_LENGTH)
+axes.xaxis.set_visible(False)
+axes.yaxis.set_visible(False)
+
+
+def get_new_data_points(packet_reader):
+    """
+    Return all data points in the buffer waiting to be displayed.
+
+    In between refreshes of the graph, data points will be
+    read into a waiting buffer where they are held before
+    they are displayed during the next refresh. The packet
+    reader is responsible for managing that buffer.
+    """
+    SAMPLES_PER_004_SECOND = int(5)
+    while True:
+        new_data = np.array([DOTS_PER_STRIP_HEIGHT / 2
+                             for _ in range(SAMPLES_PER_004_SECOND)])
+        for i, _ in enumerate(range(SAMPLES_PER_004_SECOND)):
+            channel_values = next(packet_reader)
+            if channel_values:
+                channel_1, *_ = channel_values
+                new_data[i] = channel_1
+        new_data = scipy.ndimage.zoom(new_data, 1, order=0)
+        yield new_data
+
+
+def axes_updater(axes, packet_reader):
     """
     Update exg figure.
 
     This function will update the exg figure.
 
     :param axes:
-    :param reader:
+    :param packet_reader:
     """
-    samples_per_graph = 2560
-    period_multiplier = 2
+    minor_grid_points = np.arange(0, DOTS_PER_STRIP_LENGTH, DOTS_PER_004_SECOND)
+    axes.vlines(minor_grid_points, 0, DOTS_PER_STRIP_HEIGHT, color='r', alpha=0.3)
+    major_grid_points = np.arange(0,DOTS_PER_STRIP_LENGTH, DOTS_PER_02_SECOND)
+    axes.vlines(major_grid_points, 0, DOTS_PER_STRIP_HEIGHT, color='r', alpha=0.9)
 
-    axes.set_ylim(0, 1024)
-    axes.xaxis.set_visible(False)
-    axes.yaxis.set_visible(False)
+    # Start the graph off with a flat vertically-centered line
+    ydata = np.array([DOTS_PER_STRIP_HEIGHT / 2
+                      for _ in range(DOTS_PER_STRIP_LENGTH)])
+    line, = axes.plot(range(DOTS_PER_STRIP_LENGTH), ydata)
 
-    # initialize plot with zeros
-    data = np.zeros(samples_per_graph)
-    line, = axes.plot(data)
-
-    # A counter to keep track of how many times the
-    # axes on the figure has been refreshed.
-    refresh_counter = 0
-
+    new_data_gen = get_new_data_points(packet_reader)
     while True:
-        new_packets = []
-        # Calculate the number of new packets that need to be
-        # read during this refresh.
-        packet_grab_count = 6 if refresh_counter % 4 else 7
-        refresh_counter += 1
-        for _ in range(packet_grab_count):
-            channel_values = next(reader)
-            if channel_values:
-                channel_1, *_ = channel_values
-                # Transform graph, stretch the x axis
-                for _ in range(period_multiplier):
-                    new_packets.append(channel_1)
+        new_data = next(new_data_gen)
 
-        data = np.concatenate((data[len(new_packets):], np.array(new_packets)))
-
-        # Add new data to plot
-        line.set_ydata(data)
-
-        # TODO
-        # Calculate heart rate
-        heart_rate = calculate_heart_rate(data)
-        print(heart_rate)
-        # ax.text(0.98, 0.95, '{} HR'.format(heart_rate),
-        #         fontsize=15, transform=ax.transAxes, horizontalalignment='right')
-
-        # Calibrate y axis limits
-        amax = np.amax(data)
-        amin = np.amin(data)
-        if amin != amax:
-            total_amplitude = amax - amin
-            margin = total_amplitude * 0.1
-            axes.set_ylim(amin - margin, amax + margin)
+        # Remove old data points, add new ones
+        ydata = np.delete(ydata, range(len(new_data)))
+        ydata = np.append(ydata, new_data)
+        line.set_ydata(ydata)
         yield
 
 
@@ -94,9 +112,9 @@ def show_exg(source, source_type='port'):
 
     reader = PacketStreamReader(serial_obj)
 
-    fig, axes = plt.subplots()
-    axes_refresher = axes_refresher_generator(axes, reader)
-    animation.FuncAnimation(fig, lambda _: next(axes_refresher), interval=50)
+    axes_updater_gen = axes_updater(axes, reader)
+    animation.FuncAnimation(fig, lambda _: next(axes_updater_gen),
+                            interval=REFRESH_INTERVAL_MS)
     plt.show()
 
 
